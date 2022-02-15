a4ea3278388dfa4a6a5850d753db191f7a87208d
fayeBabacar78 <babacar.faye09@univ-thies.sn>
2022-02-15 20:06:23 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clientId: 'APPLICATION-ID (CLIENT-ID)',
   webAPI: 'YOUR-WEB-API-URL',
-  redirectUri: 'CLIENT-REDIRECT-URL (THE SAME CONFIGURE IN AZURE AD FOR YOUR SPA)',
+  redirectUri: 'CLIENT-REDIRECT-URL (THE SAME CONFIGURED IN AZURE AD FOR YOUR SPA)',
   authority: 'https://login.windows.net/PUT-YOUR-TENANT-ID-HERE',
   msGraphAPI: 'https://graph.microsoft.com'
 };
